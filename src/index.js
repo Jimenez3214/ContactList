9e7106cf9acd58499aa8f31bb836053da4a170d3
Dragonsofash <agpaoajonathanb@gmail.com>
2023-05-22 21:21:49 -0400

    Added an additional test.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './components/Main.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/Main.jsx";
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-);
-//test 123
+ReactDOM.render(<Main />, document.getElementById("app"));
+//test 123. This is an additional test :)
