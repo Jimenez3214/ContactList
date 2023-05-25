5299cbc910983bdb97c8c12a456ed35a67598c62
Jimenez3214 <jimenezeduardo83@gmail.com>
2023-05-24 20:10:55 -0500

    added ContactRow.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import dummyContacts from '../dummyData';
+import ContactList from './ContactList'
 
 const Main = () => {
-  const [contacts, setContacts] = useState(dummyContacts)
+  const [contacts, setContacts] = useState(dummyContacts);
   {
     contacts.map(contact => {
       return contact.name
@@ -14,24 +15,7 @@ const Main = () => {
         <div>Contact List</div>
       </div>
       <div id="container">
-        <table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Email</th>
-            </tr>
-            {
-              contacts.map(contact => {
-                return <tr key={contact.id}>
-                  <td>{contact.name}</td>
-                  <td>{contact.phone}</td>
-                  <td>{contact.email}</td>
-                </tr>
-              })
-            }
-          </tbody>
-        </table>
+      <ContactList contacts={contacts}/>
       </div>
     </div>
   );
